2d428d0f5973728fd2183b5be0737c75e4c0f92b
farhan07434 <farhan07434@gmail.com>
2026-09-12 00:18:20 +0530

    og base code

--- frontend/app.js
+++ frontend/app.js
@@ -40,7 +40,7 @@ async function exchangeCodesforTokens(code)
         }
     )
     const tokens = await response.json();
-console.log(tokens, 'laibaaaa');
+console.log(tokens);
 localStorage.setItem("accessToken", tokens.access_token);
 localStorage.setItem("idToken",tokens.id_token);
 const idToken = localStorage.getItem("idToken");
